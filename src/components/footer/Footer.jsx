367af653f2862a94389e15dae8b572cea2959724
P3REZ05 <dpanimalito@gmail.com>
2025-01-28 16:30:00 -0500

    Commit actualizacion de creditos, version 1.0.1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white text-center py-3">
-            <p>&copy; 2023 Mi Tienda. Todos los derechos reservados.</p>
-            <div>
-                <a href="#" className="text-white mx-2">Facebook</a>
-                <a href="#" className="text-white mx-2">Twitter</a>
-                <a href="#" className="text-white mx-2">Instagram</a>
+        <footer className="bg-dark text-white text-center py-4">
+            <div className="container">
+                <p className="mb-2">&copy; 2023 Mi Tienda. Todos los derechos reservados.</p>
+                <div className="d-flex justify-content-center">
+                    <a href="#" className="text-white mx-3">
+                        <i className="bi bi-facebook"></i> Facebook
+                    </a>
+                    <a href="#" className="text-white mx-3">
+                        <i className="bi bi-twitter"></i> Twitter
+                    </a>
+                    <a href="#" className="text-white mx-3">
+                        <i className="bi bi-instagram"></i> Instagram
+                    </a>
+                </div>
             </div>
         </footer>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap
+import './index.css'; // Tus estilos personalizados
+
 
 ReactDOM.render(
     <BrowserRouter>
